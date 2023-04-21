c57e041e9406a9fc2a9115f441112db326dbd342
이가현 <88700168+leeda06@users.noreply.github.com>
2023-04-21 21:28:07 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 1+2+3+4+5+6+7+8+9+0;
+        for(int i = 0; i < numbers.length; i++){
+            answer -= numbers[i];
+        }
+        return answer;
+    }
+}
