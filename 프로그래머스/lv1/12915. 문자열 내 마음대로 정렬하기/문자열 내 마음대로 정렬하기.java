737163e32792ded15643032e7033df00f2c708c5
이가현 <88700168+leeda06@users.noreply.github.com>
2023-05-24 11:43:00 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 2.01 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        
+        for (int i = 0; i < strings.length; i++) {
+            String plus = strings[i].substring(n, n + 1);
+            strings[i] = plus + strings[i];
+        }
+
+        Arrays.sort(strings);
+              
+        for (int i = 0; i < strings.length; i++) {
+            answer[i] = strings[i].substring(1);
+        }
+        
+        return answer;
+    }
+}
