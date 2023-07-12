9b56bc911d4632f970c8a5066b18c789d5cee228
이가현 <88700168+leeda06@users.noreply.github.com>
2023-07-12 11:14:10 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.18 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        ArrayList<Integer> temp = new ArrayList<>();
+        
+        for(int i = 0; i < score.length; i++) {
+            if(temp.size() < k) {
+                temp.add(score[i]);
+                Collections.sort(temp);
+                answer[i] = temp.get(0);
+                continue;
+            }
+            
+            if(temp.size() == k) {
+            	int minNum = temp.get(0);
+            	
+            	if(minNum < score[i]) {
+            		temp.remove(0);
+                    temp.add(score[i]);
+                    Collections.sort(temp);
+            	}
+            	
+            	answer[i] = temp.get(0);
+            }
+        }
+        return answer;
+    }
+}
