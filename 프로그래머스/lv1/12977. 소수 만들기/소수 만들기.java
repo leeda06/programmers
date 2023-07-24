b058a475e54f22566d3b6d2a8fd9f2bd096afa51
이가현 <88700168+leeda06@users.noreply.github.com>
2023-07-24 18:49:00 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
